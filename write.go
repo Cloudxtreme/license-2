8c50de70dd8a7ba25251dc0df2062bf504cfc17e
Chris <cpridgen0@gmail.com>
2015-12-05 22:08:10 -0500

    Condensed to one function

--- write.go
+++ write.go
@@ -11,24 +11,28 @@ type Type int
 // The different Licenses.
 const (
 	MIT = Type(iota)
+	BSD = Type(iota)
 )
 
 // Write a license with the year and name to the Writer. If there is an error
 // writing to the Writer it will be returned.
 func Write(lic Type, info *Info, out io.Writer) error {
+	// The full license
+	var full string
+
 	// Check which license we're writing
 	switch lic {
 	case MIT:
-		write(mitText, info, out)
+		full = info.replace(mitText)
+	case BSD:
+		full = info.replace(bsdText)
+	default:
+		return errors.New("write license error: invalid type")
 	} //switch
 
-	return errors.New("write license error: invalid type")
-} //Write
+	// Write the the Writer
+	_, err := out.Write([]byte(full))
 
-func write(license string, info *Info, out io.Writer) error {
-	// Write the license to the Writer
-	_, err := out.Write([]byte(info.replace(license)))
-
-	// Return any error found
+	// Return the error
 	return err
-} //write
+} //Write
